3db802445e27831d5da7e90aa86e0b1d6b17b78e
Dwuangne <tquang0809@gmail.com>
2024-07-04 22:09:40 +0700

    Add project files.

---
+++ CameraApp/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CameraApp
+{
+    partial class formCamera
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            if (disposing)
+            {
+                if (cam != null && cam.IsRunning)
+                {
+                    cam.SignalToStop();
+                    cam.WaitForStop();
+                }
+
+                if (writer != null)
+                {
+                    writer.Release();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cboCamera = new ComboBox();
+            label1 = new Label();
+            pictureBox = new PictureBox();
+            btnStart = new Button();
+            btnTakePhoto = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            btnStop = new Button();
+            btnStartRecording = new Button();
+            btnStopRecording = new Button();
+            saveFileDialog2 = new SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            cboCamera.FormattingEnabled = true;
+            cboCamera.Location = new Point(113, 21);
+            cboCamera.Name = "cboCamera";
+            cboCamera.Size = new Size(264, 23);
+            cboCamera.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Camera:";
+            // 
+            // pictureBox
+            // 
+            pictureBox.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox.Location = new Point(27, 63);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(797, 346);
+            pictureBox.TabIndex = 2;
+            pictureBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(27, 440);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(109, 34);
+            btnStart.TabIndex = 3;
+            btnStart.Text = "&Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnTakePhoto
+            // 
+            btnTakePhoto.Location = new Point(361, 440);
+            btnTakePhoto.Name = "btnTakePhoto";
+            btnTakePhoto.Size = new Size(108, 34);
+            btnTakePhoto.TabIndex = 4;
+            btnTakePhoto.Text = "Take Photo";
+            btnTakePhoto.UseVisualStyleBackColor = true;
+            btnTakePhoto.Click += btnTakePhoto_Click;
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.Filter = "Imagename|*.jpg";
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(716, 440);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(108, 34);
+            btnStop.TabIndex = 5;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnStartRecording
+            // 
+            btnStartRecording.Location = new Point(195, 440);
+            btnStartRecording.Name = "btnStartRecording";
+            btnStartRecording.Size = new Size(108, 34);
+            btnStartRecording.TabIndex = 6;
+            btnStartRecording.Text = "Start Recording";
+            btnStartRecording.UseVisualStyleBackColor = true;
+            btnStartRecording.Click += btnStartRecording_Click;
+            // 
+            // btnStopRecording
+            // 
+            btnStopRecording.Location = new Point(539, 440);
+            btnStopRecording.Name = "btnStopRecording";
+            btnStopRecording.Size = new Size(108, 34);
+            btnStopRecording.TabIndex = 7;
+            btnStopRecording.Text = "Stop Recording";
+            btnStopRecording.UseVisualStyleBackColor = true;
+            btnStopRecording.Click += btnStopRecording_Click;
+            // 
+            // saveFileDialog2
+            // 
+            saveFileDialog2.Filter = "AVI Files|*.avi";
+            // 
+            // formCamera
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(836, 486);
+            Controls.Add(btnStopRecording);
+            Controls.Add(btnStartRecording);
+            Controls.Add(btnStop);
+            Controls.Add(btnTakePhoto);
+            Controls.Add(btnStart);
+            Controls.Add(pictureBox);
+            Controls.Add(label1);
+            Controls.Add(cboCamera);
+            Name = "formCamera";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Camera";
+            Load += formCamera_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cboCamera;
+        private Label label1;
+        private PictureBox pictureBox;
+        private Button btnStart;
+        private Button btnTakePhoto;
+        private SaveFileDialog saveFileDialog1;
+        private Button btnStop;
+        private Button btnStartRecording;
+        private Button btnStopRecording;
+        private SaveFileDialog saveFileDialog2;
+    }
+}
---
+++ CameraApp/Form1.cs
@@ -0,0 +1,229 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.Diagnostics;
+
+namespace CameraApp
+{
+    public partial class formCamera : Form
+    {
+        private FilterInfoCollection cameras;
+        private VideoCaptureDevice cam;
+        private VideoWriter writer;
+        private bool isRecording = false;
+        private OpenCvSharp.Size frameSize;
+
+        public formCamera()
+        {
+            InitializeComponent();
+            LoadCameras();
+        }
+
+        private void LoadCameras() //Load camera mặc định khi bắt đầu phần mềm
+        {
+            cameras = new FilterInfoCollection(FilterCategory.VideoInputDevice);//tìm những thiết bị ghi hình kết nối với máy
+            foreach (FilterInfo info in cameras) //add thiết bị vô cboCamera để lựa chọn
+            {
+                cboCamera.Items.Add(info.Name);
+            }
+            cboCamera.SelectedIndex = 0; //luôn luôn chọn camera mặc định ở vị trí bằng 0
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (cam != null && cam.IsRunning) //Nếu dã có cam chạy rồi thì không cần khởi động lại cam
+            {
+                MessageBox.Show("Camera is already running.");
+                return;
+            }
+
+            StartCamera(); //nếu cam chưa chạy thì chạy cam
+        }
+
+        private OpenCvSharp.Size GetFrameSize()  //căn chỉnh kích thước của màn hình
+        {
+            if (cam != null && cam.VideoCapabilities.Length > 0) //nếu có cam thì lấy kích thước của cam
+            {
+                var resolution = cam.VideoCapabilities[0];
+                return new OpenCvSharp.Size(resolution.FrameSize.Width, resolution.FrameSize.Height);
+            }
+            // Nếu không lấy được kích thước từ camera, sử dụng kích thước mặc định
+            return new OpenCvSharp.Size(640, 480);
+        }
+        private void StartCamera()
+        {
+            try
+            {
+                cam = new VideoCaptureDevice(cameras[cboCamera.SelectedIndex].MonikerString); //chọn camera trong cboCamera
+                cam.NewFrame += Cam_NewFrame; // tạo New Frame
+                cam.Start(); //chạy camera
+
+                // Lấy kích thước frame từ camera
+                frameSize = GetFrameSize();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting camera: {ex.Message}");
+            }
+        }
+
+        private void Cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Tạo một bản sao của frame hiện tại từ camera
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+
+            // Sử dụng đối tượng Graphics để vẽ lên bitmap
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                // Tạo font cho văn bản: Arial, cỡ 20, in đậm, màu đỏ
+                Font font = new Font("Arial", 20, FontStyle.Bold);
+                Brush brush = new SolidBrush(Color.Red);
+                // Vẽ chuỗi thời gian hiện tại lên bitmap ở vị trí (10, 10)
+                g.DrawString(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), font, brush, new PointF(10, 10));
+            }
+
+            // Kiểm tra xem có đang ghi video không và writer đã được khởi tạo chưa
+            if (isRecording && writer != null)
+            {
+                try
+                {
+                    // Chuyển đổi bitmap sang Mat (định dạng của OpenCV)
+                    using (Mat mat = bitmap.ToMat())
+                    {
+                        // Ghi frame vào file video
+                        writer.Write(mat);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Error writing frame: {ex.Message}");
+                }
+            }
+            // Giải phóng bộ nhớ của ảnh cũ trong pictureBox (nếu có)
+            pictureBox.Image?.Dispose();
+            // Hiển thị frame mới lên pictureBox
+            pictureBox.Image = bitmap;
+        }
+
+        private void formCamera_Load(object sender, EventArgs e)
+        {
+            StartCamera(); //Load camera khi bắt đầu
+        }
+
+        private void btnTakePhoto_Click(object sender, EventArgs e)
+        {
+            //kiểm tra xem có image không
+            if (pictureBox.Image == null)
+            {
+                MessageBox.Show("No image to save.");
+                return;
+            }
+
+            //địa chỉ lưu file
+            saveFileDialog1.InitialDirectory = "D:\\Materials_FPTU\\Season_5\\PRN212_PlatformApplicationProgramming\\Code_PRN212\\Project_PRN\\File";
+            // Hiển thị hộp thoại lưu file và kiểm tra xem người dùng đã chọn vị trí lưu chưa
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Lưu hình ảnh từ pictureBox vào file được chọn
+                    pictureBox.Image.Save(saveFileDialog1.FileName);
+                    MessageBox.Show("Image saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving image: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopCamera(); //dừng camera
+        }
+
+        private void StopCamera()
+        {
+            try
+            {
+                //nếu đang có cam thì dừng lại
+                if (cam != null && cam.IsRunning)
+                {
+                    cam.SignalToStop();
+                    cam.WaitForStop();
+                    cam = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error stopping camera: {ex.Message}");
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            //ấn nút tắt 
+            base.OnFormClosing(e);
+            StopCamera();
+            StopRecording();
+        }
+
+        private void btnStartRecording_Click(object sender, EventArgs e)
+        {
+            //Nếu đang trong trạng thái record
+            if (isRecording)
+            {
+                MessageBox.Show("Already recording.");
+                return;
+            }
+            //thiết lập kiểu file muốn lưu(có thể không cần vì đã cài trươc)
+            saveFileDialog2.Filter = "AVI Files|*.avi";
+            //địa chỉ lưu file
+            saveFileDialog2.InitialDirectory = "D:\\Materials_FPTU\\Season_5\\PRN212_PlatformApplicationProgramming\\Code_PRN212\\Project_PRN\\File";
+            //mở folder cần lưu file
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Tạo mã FourCC cho codec video XVID
+                    int fourcc = VideoWriter.FourCC('X', 'V', 'I', 'D');
+
+                    // Khởi tạo đối tượng VideoWriter để ghi video
+                    // Tham số:
+                    // - saveFileDialog2.FileName: Đường dẫn và tên file video sẽ được lưu
+                    // - fourcc: Mã codec video
+                    // - 25: Số khung hình trên giây (fps)
+                    // - frameSize: Kích thước khung hình
+                    writer = new VideoWriter(saveFileDialog2.FileName, fourcc, 25, frameSize);
+                    isRecording = true;
+                    MessageBox.Show("Recording started.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error starting recording: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnStopRecording_Click(object sender, EventArgs e)
+        {
+            StopRecording();
+        }
+
+        private void StopRecording()
+        {
+            //nếu đang record thì dừng lại
+            if (isRecording)
+            {
+                isRecording = false;
+                //để giải phóng các tài nguyên liên quan đến video, như đóng file video đang ghi.
+                writer?.Release();
+                //đảm bảo hệ thống được giải phóng đúng cách
+                writer?.Dispose();
+                writer = null;
+                MessageBox.Show("Recording stopped.");
+            }
+        }
+    }
+}
